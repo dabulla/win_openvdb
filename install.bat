da3da1b749057827ba37b8b7dae7133000fb10e4
Daniel Bulla <info@danielbulla.de>
2016-06-07 00:10:59 +0200

    updated install script to use patched openexr

--- install.bat
+++ install.bat
@@ -4,6 +4,7 @@ echo Initialize project %1 with openvdb. Should work with vc2012, vc2013 and vc2
 mkdir "%1"
 cd "%1"
 set /p="Set up openvdb for %cd%. Hit Enter..."
+set /p CONF="Please type "Debug" or "Release" to choose a configuration -> "
 echo.
 echo MANUAL ACTIONS REQUIRED: Go to following websites and download the correct package for you"
 echo Q: Why should I use Linux instead?"
@@ -64,7 +65,7 @@ echo Cloning repositories...
 git clone https://github.com/dabulla/win_openvdb
 echo Clone OpenVDB Done.
 echo.
-git clone https://github.com/rchoetzlein/win_openexr.git
+git clone https://github.com/dabulla/win_openexr.git
 echo Clone OpenEXR/IlmBase Done.
 echo.
 git clone https://github.com/rchoetzlein/zlib.git
@@ -75,7 +76,7 @@ echo.
 set /p MY_CMAKE="please type the path to your cmake.exe. E.g.: "C:\Program Files (x86)\CMake\bin\cmake.exe""
 "%MY_CMAKE%" --help
 set /p MY_CMAKE_GEN=Choose a generator from the above list. Note: Only supports x64, so add "Win64" (please copy paste name e.g. Visual Studio 12 Win64)
-set /p MY_DEVENV=Specify full path of devenv.exe (with quotation marks). E.g. "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe"
+set /p MY_DEVENV=Specify full path of devenv.exe (with quotation marks). E.g. "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com"
 rem set PATH=%MY_DEVENV%;%PATH%
 cd ..\build
 mkdir zlib
@@ -83,7 +84,7 @@ cd zlib
 echo Generate zlib - project for %MY_CMAKE_GEN%
 "%MY_CMAKE%" -G "%MY_CMAKE_GEN%" ..\..\source\zlib
 echo Compiling zlib
-"%MY_DEVENV%" zlib.sln /build "Debug" /project ALL_BUILD
+rem "%MY_DEVENV%" zlib.sln /build "%CONF%" /project ALL_BUILD only release works
 "%MY_DEVENV%" zlib.sln /build "Release" /project ALL_BUILD
 
 cd ..
@@ -92,8 +93,7 @@ cd IlmBase
 echo Generate IlmBase - project for %MY_CMAKE_GEN%
 "%MY_CMAKE%" -G "%MY_CMAKE_GEN%" ..\..\source\win_openexr\IlmBase
 echo Compiling IlmBase
-"%MY_DEVENV%" /build "Debug" IlmBase.sln /project ALL_BUILD
-"%MY_DEVENV%" /build "Release" IlmBase.sln /project ALL_BUILD
+"%MY_DEVENV%" /build "%CONF%" IlmBase.sln /project ALL_BUILD
 
 cd ..
 mkdir OpenEXR
@@ -101,8 +101,7 @@ cd OpenEXR
 echo Generate OpenEXR - project for %MY_CMAKE_GEN%
 "%MY_CMAKE%" -G "%MY_CMAKE_GEN%" ..\..\source\win_openexr\OpenEXR
 echo Compiling OpenEXR
-"%MY_DEVENV%" /build "Debug" OpenEXR.sln /project ALL_BUILD
-"%MY_DEVENV%" /build "Release" OpenEXR.sln /project ALL_BUILD
+"%MY_DEVENV%" /build "%CONF%" OpenEXR.sln /project ALL_BUILD
 
 cd ..
 mkdir OpenVDB
@@ -110,7 +109,6 @@ cd OpenVDB
 echo Generate OpenVDB - project for %MY_CMAKE_GEN%
 "%MY_CMAKE%" -G "%MY_CMAKE_GEN%" ..\..\source\win_openvdb\OpenVDB
 echo Finally compiling OpenVDB
-"%MY_DEVENV%" /build "Debug" OpenVDB.sln /project ALL_BUILD
-"%MY_DEVENV%" /build "Release" OpenVDB.sln /project ALL_BUILD
+"%MY_DEVENV%" /build "%CONF%" OpenVDB.sln /project ALL_BUILD
 echo Done!
 cd ..\..\..\..
